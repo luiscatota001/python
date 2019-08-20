4fa66e2046ef0a94234683b9d338aac9548b2306
luiscatota001 <luiscatota001@gmail.com>
2019-08-20 11:13:42 -0500

    version 2

---
+++ aplicacion.py
@@ -0,0 +1,20 @@
+'''
+x = 6
+y = 9
+print(x+y)
+n = "santiago"
+a = 'solis'
+e = 35
+print("hola "+n+" tu aplelido es "+a+" y tienes "+str (e)+"años")
+print ("hola {} tu apellido es {} y tienes {} años".format(n,a,e))
+print(n.upper())
+'''
+f = (float) (input("ingrese un numero: "))
+g = (float) (input("ingrese otro numero: "))
+print("la suma de {} + {} = {}".format(f,g,(f+g)))
+print("la resta de {} - {} = {}".format(f,g,(f-g)))
+print("la multiplicacion  de {} * {} = {}".format(f,g,(f*g)))
+print("la division de {} / {} = {}".format(f,g,(f/g)))
+b =  True
+print(type(b))
+
